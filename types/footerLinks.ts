7edf22d2b8f7d91a3d7489e5cb14be821bda3187
ejim11 <favourejim56@gmail.com>
2025-02-15 20:12:04 +0100

    🎨 added footer links data

---
+++ types/footerLinks.ts
@@ -0,0 +1,41 @@
+export const footerLinks: {
+  title: string;
+  links: { text: string; link: string }[];
+}[] = [
+  {
+    title: "company",
+    links: [
+      { text: "about", link: "/about" },
+      { text: "features", link: "/features" },
+      { text: "works", link: "/works" },
+      { text: "career", link: "/career" },
+    ],
+  },
+  {
+    title: "help",
+    links: [
+      { text: "customer support", link: "/customer-support" },
+      { text: "delivery details", link: "/delivery-details" },
+      { text: "terms & conditions", link: "/terms-conditions" },
+      { text: "privacy policy", link: "/privacy-policy" },
+    ],
+  },
+  {
+    title: "faq",
+    links: [
+      { text: "account", link: "/account" },
+      { text: "manage deliveries", link: "/manage-deliveries" },
+      { text: "orders", link: "/orders" },
+      { text: "payments", link: "/payments" },
+    ],
+  },
+  {
+    title: "resources",
+    links: [
+      { text: "free eBooks", link: "/free-ebooks" },
+      { text: "development tutorial", link: "/development-tutorial" },
+      { text: "how to-blog", link: "/how-to-blog" },
+      { text: "youtube playlist", link: "/payments" },
+    ],
+  },
+];
