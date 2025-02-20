29fe4a6090718120383ace149dddd7e2b21aff73
ejim11 <favourejim56@gmail.com>
2025-02-20 14:25:09 +0100

    💄 added custom classname prop

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -1,7 +1,18 @@
 import React, { ReactNode } from "react";
 
-const MainContainer: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return <main className="bg-[#FFFBFB] px-[10rem] py-[7rem]">{children}</main>;
+const MainContainer: React.FC<{ children: ReactNode; classname?: string }> = ({
+  children,
+  classname,
+}) => {
+  return (
+    <main
+      className={` ${
+        classname ? classname : ""
+      } bg-[#FFFBFB] px-[10rem] py-[7rem]`}
+    >
+      {children}
+    </main>
+  );
 };
 
 export default MainContainer;
