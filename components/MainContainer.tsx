773f32ecbbc347e03858db5d08b639352460df22
ejim11 <favourejim56@gmail.com>
2025-02-20 12:20:33 +0100

    💄 removed the min-height iof screen for the main container

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -1,11 +1,7 @@
 import React, { ReactNode } from "react";
 
 const MainContainer: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return (
-    <main className="bg-[#FFFBFB] px-[10rem] py-[7rem] min-h-screen">
-      {children}
-    </main>
-  );
+  return <main className="bg-[#FFFBFB] px-[10rem] py-[7rem]">{children}</main>;
 };
 
 export default MainContainer;
