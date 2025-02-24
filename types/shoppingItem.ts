b3da3bf013b6c6d44dccc7943c888d423c830770
ejim11 <favourejim56@gmail.com>
2025-02-24 13:46:21 +0100

    🏷️ updated the ShoppingItem type

--- types/shoppingItem.ts
+++ types/shoppingItem.ts
@@ -1,9 +1,12 @@
 import { StaticImageData } from "next/image";
 
 export type ShoppingItem = {
+  id?: number;
   image: StaticImageData;
   title: string;
   price: number;
   discount: number;
   soldOut?: boolean;
+  type?: string;
+  sizes?: string[];
 };
