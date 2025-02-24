a712ebda580d95ff3d67b1784c22fc0c5ffed1b2
ejim11 <favourejim56@gmail.com>
2025-02-24 13:39:25 +0100

    ✅ added the collections comp

--- app/collections/page.tsx
+++ app/collections/page.tsx
@@ -1,5 +1,6 @@
+import CollectionsComp from "@/components/Collections/CollectionsComp";
 import React from "react";
 
 export default function Collections() {
-  return <div></div>;
+  return <CollectionsComp />;
 }
