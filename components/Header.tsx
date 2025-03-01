3ce18bc96ca739003c7c174409dee2a5f0263da2
ejim11 <favourejim56@gmail.com>
2025-03-01 22:32:02 +0100

    ✅ Added signup to the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -58,59 +58,67 @@ const Header = () => {
   }, [checkout]);
 
   return (
-    <header className="flex h-[7.5rem] fixed top-0 left-0 right-0 z-50 items-center px-[8rem] bg-color-white justify-between border-b border-[#F0EEED]">
-      <div className="w-[9.3rem] h-[4.3rem]">
-        <Image
-          src={logo}
-          alt=" logo image"
-          priority
-          width={200}
-          height={100}
-          className="w-full h-full"
-        />
-      </div>
-      <div>
-        <nav>
-          <ul className="flex items-center font-satoshi capitalize text-[1.8rem] text-color-black">
-            {headerNavLinks.map((navItem: HeaderNavLink) => (
-              <li key={navItem.title} className="mr-[5rem] last:mr-0">
-                <HeaderNavigationLink navItem={navItem} />
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-      <div className="flex items-center text-color-black">
-        <button>
-          <LuSearch className="w-[2.4rem] h-[2.4rem] text-color-current" />
-        </button>
-        <button className="relative ml-[1.4rem] mr-[2rem]">
-          <LuShoppingCart className="w-[2.4rem] h-[2.4rem] text-color-current " />
-          <span
-            className={`${checkout.length > 0 ? "flex " : "hidden"} ${
-              scaleUp ? "scale-125" : "scale-100"
-            } text-white -top-[1rem] -right-[0.5rem] bg-red-600 w-[2rem] h-[2rem] text-[1.2rem] rounded-full absolute items-center justify-center transition-all ease-in duration-200`}
-          >
-            {checkout.length}
-          </span>
-        </button>
-        <div className="hidden">
-          <button className="flex items-center ">
-            <FaRegUserCircle className="w-[2.4rem] h-[2.4rem] text-color-current mr-[0.3rem]" />
-            <FiChevronDown className="w-[1.6rem] h-[1.6rem] text-color-current" />
-          </button>
-          <p className="font-satoshi ml-[1.5rem]">NA</p>
-        </div>
-        <Link
-          href={"/login"}
-          className="bg-black text-white px-[2rem] py-[1rem] rounded-[0.6rem]"
-        >
-          Login
+    <div className="w-full flex flex-col fixed top-0 left-0 right-0 z-50">
+      <div className="flex w-full h-[3.8rem] items-center bg-black text-color-white justify-center font-satoshi text-[1.4rem] leading-[1.8rem]">
+        <p>Sign up and get 20% off to your first order. </p>
+        <Link href={"/register"} className="ml-[0.5rem] font-medium underline">
+          Sign Up Now
         </Link>
-
-        {/* <div className="fixed top-[8rem] right-[5rem] w-[20rem] h-[20rem] shadow-lg bg-white"></div> */}
       </div>
-    </header>
+      <header className="flex h-[7.5rem]   items-center px-[8rem] bg-color-white justify-between border-b border-[#F0EEED]">
+        <div className="w-[9.3rem] h-[4.3rem]">
+          <Image
+            src={logo}
+            alt=" logo image"
+            priority
+            width={200}
+            height={100}
+            className="w-full h-full"
+          />
+        </div>
+        <div>
+          <nav>
+            <ul className="flex items-center font-satoshi capitalize text-[1.8rem] text-color-black">
+              {headerNavLinks.map((navItem: HeaderNavLink) => (
+                <li key={navItem.title} className="mr-[5rem] last:mr-0">
+                  <HeaderNavigationLink navItem={navItem} />
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+        <div className="flex items-center text-color-black">
+          <button>
+            <LuSearch className="w-[2.4rem] h-[2.4rem] text-color-current" />
+          </button>
+          <button className="relative ml-[1.4rem] mr-[2rem]">
+            <LuShoppingCart className="w-[2.4rem] h-[2.4rem] text-color-current " />
+            <span
+              className={`${checkout.length > 0 ? "flex " : "hidden"} ${
+                scaleUp ? "scale-125" : "scale-100"
+              } text-white -top-[1rem] -right-[0.5rem] bg-red-600 w-[2rem] h-[2rem] text-[1.2rem] rounded-full absolute items-center justify-center transition-all ease-in duration-200`}
+            >
+              {checkout.length}
+            </span>
+          </button>
+          <div className="hidden">
+            <button className="flex items-center ">
+              <FaRegUserCircle className="w-[2.4rem] h-[2.4rem] text-color-current mr-[0.3rem]" />
+              <FiChevronDown className="w-[1.6rem] h-[1.6rem] text-color-current" />
+            </button>
+            <p className="font-satoshi ml-[1.5rem]">NA</p>
+          </div>
+          <Link
+            href={"/login"}
+            className="bg-black text-white px-[2rem] py-[1rem] rounded-[0.6rem]"
+          >
+            Login
+          </Link>
+
+          {/* <div className="fixed top-[8rem] right-[5rem] w-[20rem] h-[20rem] shadow-lg bg-white"></div> */}
+        </div>
+      </header>
+    </div>
   );
 };
 
