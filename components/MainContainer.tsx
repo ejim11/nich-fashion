c1419ce785ae2eee24924470449de2d565085004
ejim11 <favourejim56@gmail.com>
2025-03-01 22:33:16 +0100

    💄 corrected the padding styling in the main container

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -1,15 +1,15 @@
 import React, { ReactNode } from "react";
 
-const MainContainer: React.FC<{ children: ReactNode; classname?: string; customPadding?: string }> = ({
-  children,
-  classname,
-  customPadding
-}) => {
+const MainContainer: React.FC<{
+  children: ReactNode;
+  classname?: string;
+  customPadding?: string;
+}> = ({ children, classname, customPadding }) => {
   return (
     <main
-      className={` ${
-        classname ? classname : ""
-      } ${customPadding? customPadding:"px-[10rem] py-[7rem]"} bg-[#FFFBFB] px-[10rem] py-[7rem]`}
+      className={` ${classname ? classname : ""} ${
+        customPadding ? customPadding : "px-[10rem] py-[10rem]"
+      } bg-[#FFFBFB] `}
     >
       {children}
     </main>
