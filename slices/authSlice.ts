0457d52342cf3ac290523385e95002ff4080ba81
ejim11 <favourejim56@gmail.com>
2025-03-03 20:25:00 +0100

    ✨ added the auth slice for managing auth states

---
+++ slices/authSlice.ts
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { retrieveStoredToken } from "@/utils/calculateExpirationTime";
+import { createSlice } from "@reduxjs/toolkit";
+
+// retrieving the stored token, investor details and expiration time
+const tokenData: any = retrieveStoredToken();
+let storedToken, storedRefreshToken, storedDuration;
+if (tokenData) {
+  storedToken = tokenData.token;
+  storedRefreshToken = tokenData.refreshToken;
+  storedDuration = tokenData.duration;
+}
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    isLoggedIn: !!storedToken || false,
+    token: storedToken || "",
+    remainingTime: storedDuration || 0,
+    refreshToken: storedRefreshToken || "",
+    otp: "",
+  },
+  reducers: {
+    setUserTokens(state: any, action: { payload: any }) {
+      state.token = action.payload.accessToken;
+      state.refreshToken = action.payload.refreshToken;
+      state.isLoggedIn = true;
+      localStorage.setItem("expirationTime", action.payload.expirationTime);
+      localStorage.setItem("userToken", action.payload.accessToken);
+      localStorage.setItem("userRefreshToken", action.payload.refreshToken);
+    },
+    // logout handler
+    logoutHandler(state: any, action: { payload: any }) {
+      state.token = "";
+      state.isLoggedIn = false;
+      state.refreshToken = "";
+      emptyLocalStorage();
+
+      if (action.payload.logoutTimer) {
+        clearTimeout(action.payload.logoutTimer);
+      }
+    },
+    autoLogoutHandler(state: any) {
+      state.token = "";
+      state.isLoggedIn = false;
+      state.refreshToken = "";
+      emptyLocalStorage();
+    },
+    setResetOtp(state, action) {
+      state.otp = action.payload;
+    },
+  },
+});
+
+function emptyLocalStorage() {
+  localStorage.removeItem("userToken");
+  localStorage.removeItem("userRefreshToken");
+  localStorage.removeItem("expirationTime");
+  localStorage.removeItem("user");
+  localStorage.removeItem("dashboardItems");
+}
+
+export const authActions = authSlice.actions;
+export default authSlice;
