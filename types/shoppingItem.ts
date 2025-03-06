1b958fa4894d847c3a7f0a868df7cc1a23b3fc7c
ejim11 <favourejim56@gmail.com>
2025-03-06 07:12:12 +0100

    🏷️ updated the shopping item types

--- types/shoppingItem.ts
+++ types/shoppingItem.ts
@@ -1,12 +1,37 @@
 import { StaticImageData } from "next/image";
 
+type Color = {
+  color: string;
+  quantity: number;
+};
+
+type Size = {
+  quantity: number;
+  size: string;
+};
+
+export type Review = {
+  id: string;
+  reviewer: string;
+  comment: string;
+  dateCreated: Date;
+  stars: number;
+};
+
 export type ShoppingItem = {
-  id?: number;
+  id: string;
   image: StaticImageData;
-  title: string;
+  name: string;
   price: number;
   discount: number;
-  soldOut?: boolean;
-  type?: string;
-  sizes?: string[];
+  soldOut: boolean;
+  type: string;
+  material: string;
+  brand: string;
+  sizes: Size[];
+  shortDescription: string;
+  longDescription: string[];
+  colors: Color[];
+  otherImages: StaticImageData[];
+  reviews?: Review[];
 };
