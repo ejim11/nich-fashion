3da1dd4338c610fc235f108037a6141917cbcd0b
ejim11 <favourejim56@gmail.com>
2025-03-10 17:58:48 +0100

    ✅ created the item description for the shopping item

--- components/Collections/ShoppingItemDetail/ItemDescription.tsx
+++ components/Collections/ShoppingItemDetail/ItemDescription.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const ItemDescription = () => {
-  return <div>ItemDescription</div>;
+const ItemDescription = ({ texts }: { texts: string[] }) => {
+  return (
+    <div className="mt-[4rem]">
+      {texts.map((text: string, i: number) => (
+        <p key={i} className="pb-[1rem] font-satoshi leading-[2.8rem]">
+          {text}
+        </p>
+      ))}
+    </div>
+  );
 };
 
 export default ItemDescription;
