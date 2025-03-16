f106e59ad5bde00b61cd7248a26756a67d20e348
ejim11 <favourejim56@gmail.com>
2025-03-16 23:56:15 +0100

    🗃️ updated collections data

--- data/collections.ts
+++ data/collections.ts
@@ -689,7 +689,7 @@ export const collections: ShoppingItem[] = [
     dressStyle: "casual",
   },
   {
-    id: " 6",
+    id: "6",
     image: topSellingImg6,
     name: "Cow neck Hoodie",
     price: 260,
@@ -756,7 +756,6 @@ export const collections: ShoppingItem[] = [
       "Korem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.",
       "Korem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.",
     ],
-
     colors: [
       {
         color: "pink",
