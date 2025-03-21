91854a1711d60dc0873479b41d5a95d6b5f24b62
ejim11 <favourejim56@gmail.com>
2025-03-21 01:01:33 +0100

    🦺 added fullname input validation

--- utils/inputValidator.ts
+++ utils/inputValidator.ts
@@ -1,4 +1,19 @@
 export const registrationOption = {
+  fullname: {
+    required: "Fullname is required",
+    minLength: {
+      value: 2,
+      message: "Name must have at least 2 characters",
+    },
+    maxLength: {
+      value: 98,
+      message: "Name cannot be greater than 98 characters",
+    },
+    pattern: {
+      value: /^[A-Z][a-z]+\s[A-Z][a-z]+$/i,
+      message: "Fullname is required",
+    },
+  },
   firstName: {
     required: "First name is required",
     minLength: {
