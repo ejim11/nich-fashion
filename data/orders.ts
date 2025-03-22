35757116cf599106c661cf2c204eafea3d4dd568
ejim11 <favourejim56@gmail.com>
2025-03-22 23:56:38 +0100

    🗃️ added the orders data

---
+++ data/orders.ts
@@ -0,0 +1,23 @@
+import { OrderState } from "@/enums/orderState.enum";
+import { OrderItem } from "@/types/orderItem";
+
+export const orders: OrderItem[] = [
+  {
+    id: "FW-28678678",
+    createdAt: new Date().toISOString(),
+    estimatedDeliveryTime: new Date().toISOString(),
+    carrier: "Guo ltd logistics",
+    carrierPhoneNumber: "07023287382892",
+    shippingAddress: "12 Coker, Lagos",
+    orderState: OrderState.PLACED,
+  },
+  {
+    id: "FW-28382901",
+    createdAt: new Date().toISOString(),
+    estimatedDeliveryTime: new Date().toISOString(),
+    carrier: "abc logistic",
+    carrierPhoneNumber: "0902428292192",
+    shippingAddress: "12 Okey Amazi , Idumota, Lagos",
+    orderState: OrderState.PLACED,
+  },
+];
