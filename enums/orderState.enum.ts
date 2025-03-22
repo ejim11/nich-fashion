a5a90b17ab4d5c61c777a3b0922f829df1b6f694
ejim11 <favourejim56@gmail.com>
2025-03-22 23:57:14 +0100

    🏷️ added the OrderState enum

---
+++ enums/orderState.enum.ts
@@ -0,0 +1,7 @@
+export enum OrderState {
+  PLACED = "order-placed",
+  PROCESSING = "order-processing",
+  SHIPPED = "order-shipped",
+  OUTFORDELIVERY = "order-out-for-delivery",
+  DELIVERED = "delivered",
+}
