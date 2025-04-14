36c76cf718a865cabb1046349ad80724649a4ce1
ejim11 <favourejim56@gmail.com>
2025-04-14 05:16:27 +0100

    🔧  added the base image url to the next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL("https://d3bcnu5rxir0tz.cloudfront.net/**")],
+  },
 };
 
 export default nextConfig;
