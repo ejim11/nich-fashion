49aa7ba6aee455917aa9509f593e939df3de5642
ejim11 <favourejim56@gmail.com>
2025-04-14 05:26:58 +0100

    ✅ formatted the amounts

--- components/Collections/CollectionItem.tsx
+++ components/Collections/CollectionItem.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 // import { useAppDispatch, useAppSelector } from "@/hooks/stateHooks";
 // import { checkoutActions } from "@/slices/checkoutSlice";
 import Link from "next/link";
+import formatAmount from "@/utils/formatAmount";
 
 const CollectionItem = ({ collection }: { collection: ShoppingItem }) => {
   // const dispatch = useAppDispatch();
@@ -43,7 +44,7 @@ const CollectionItem = ({ collection }: { collection: ShoppingItem }) => {
         />
       </div>
       <div className="flex w-full items-start mt-[1rem]">
-        <p className=" text-[1.8rem] font-bold font-satoshi mr-[1rem]">
+        <p className=" text-[1.8rem] font-bold font-satoshi mr-[1rem] capitalize">
           {collection.name}
         </p>
         {/* <button
@@ -63,15 +64,19 @@ const CollectionItem = ({ collection }: { collection: ShoppingItem }) => {
       <div className="text-[1.8rem] leading-[3.1rem] font-bold flex items-center">
         <p className="font-satoshi text-color-black   mr-[1rem]">
           {collection.discount > 0
-            ? `$${
-                collection.price -
-                collection.price * (collection.discount / 100)
-              }`
-            : `$${collection.price}`}
+            ? `N${formatAmount(
+                String(
+                  collection.price -
+                    collection.price * (collection.discount / 100)
+                )
+              )}`
+            : `N${formatAmount(String(collection.price))}`}
         </p>
         {collection.discount > 0 && (
           <div className="flex items-center">
-            <p className="text-[#00000066] line-through">${collection.price}</p>
+            <p className="text-[#00000066] line-through">
+              N{formatAmount(String(collection.price))}
+            </p>
             <p className="bg-color-red-2 text-color-red-1 text-[1rem] ml-[1rem] rounded-[6rem] px-[1.2rem] leading-[1.5rem] py-[0.5rem]  font-medium font-satoshi">
               -{collection.discount}%
             </p>
