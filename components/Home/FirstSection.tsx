b0d8c27567218d5e0491cdde130eeffd4fab0978
ejim11 <favourejim56@gmail.com>
2025-04-24 16:37:41 +0100

    ✅ made the comp responsive

--- components/Home/FirstSection.tsx
+++ components/Home/FirstSection.tsx
@@ -8,9 +8,9 @@ import Link from "next/link";
 
 const FirstSection = () => {
   return (
-    <section className="h-[77rem] w-full rounded-tl-[3rem] rounded-tr-[3rem] bg-color-grey-3 px-[7.2rem] flex items-end">
-      <div className="w-full flex relative">
-        <div className="w-[36.5rem] h-[60rem]">
+    <section className="h-[77rem] smd:h-auto w-full rounded-tl-[3rem] rounded-tr-[3rem] bg-color-grey-3 px-[7.2rem] 2xl:px-[3rem] xl:px-[1rem]  xlg:px-[0.2rem]  flex items-end">
+      <div className="w-full flex relative  smd:flex-wrap smd:items-center smd:flex-col">
+        <div className="w-[36.5rem] h-[60rem] smd:order-2  smd:mt-[3rem] sm:h-auto">
           <Image
             src={firstSecImg1}
             alt="first section image of a person"
@@ -20,11 +20,11 @@ const FirstSection = () => {
             className="w-full h-full"
           />
         </div>
-        <div className="flex-1  self-stretch text-center pt-[4rem] flex flex-col items-center">
-          <h1 className="font-monserrat text-[5.4rem]  font-black leading-[6.4rem]">
+        <div className="smd:flex-[100%] smd:order-1 flex-1  self-stretch xlg:self-center text-center pt-[4rem] xlg:px-[1rem] flex flex-col items-center ">
+          <h1 className="font-monserrat text-[5.4rem] 3xl:text-[5rem] xl:text-[4rem] smd:text-[3rem]  font-black leading-[6.4rem] smd:leading-[4rem] ">
             FIND CLOTHES THAT MATCHES YOUR STYLE
           </h1>
-          <p className="font-satoshi leading-[2.2rem] text-[#00000099] my-[2.6rem]">
+          <p className="font-satoshi leading-[2.2rem] text-[#00000099] my-[2.6rem] ">
             Browse through our diverse range of meticulously crafted garments,
             designed to bring out your individuality and cater to your sense of
             style.
@@ -36,7 +36,7 @@ const FirstSection = () => {
             Shop Now
           </Link>
         </div>
-        <div className="w-[27rem] h-[60rem]">
+        <div className="w-[27rem] h-[60rem] xlg:hidden">
           <Image
             src={firstSecImg2}
             alt="first section image of a person"
@@ -46,20 +46,20 @@ const FirstSection = () => {
             className="w-full h-full"
           />
         </div>
-        <div className="w-[5.6rem] h-[5.6rem] absolute bottom-[7.6rem] left-[40rem]">
+        <div className="w-[5.6rem] h-[5.6rem] absolute bottom-[7.6rem] left-[40rem] smd:left-[55rem] sm:right-0  sm:left-auto  ">
           <Image
             src={firstSecImg3}
-            alt="first section image of a person"
+            alt="first section image of a star"
             priority
             width={1000}
             height={1000}
             className="w-full h-full"
           />
         </div>
-        <div className="w-[7.8rem] h-[7.8rem] absolute right-[15rem] -top-[8rem]">
+        <div className="w-[7.8rem] h-[7.8rem] absolute right-[15rem] smd:left-[10rem] smd:right-auto sm:top-[30rem] -top-[8rem] smd:top-[18rem] sm:left-0">
           <Image
             src={firstSecImg4}
-            alt="first section image of a person"
+            alt="first section image of a star icon"
             priority
             width={1000}
             height={1000}
