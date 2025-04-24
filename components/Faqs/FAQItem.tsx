5c613b9a6ba8092628dfb3742c2b1be851faee97
ejim11 <favourejim56@gmail.com>
2025-04-24 17:25:37 +0100

    📱 made responsive

--- components/Faqs/FAQItem.tsx
+++ components/Faqs/FAQItem.tsx
@@ -36,7 +36,7 @@ const FAQITem = ({
         onClick={onToggleDisplayEventInfoHandler}
         initial={false}
       >
-        <span className="font-bold text-color-black text-[2.2rem] leading-[2.8rem] font-satoshi  smd:text-left">
+        <span className="flex-1 font-bold text-color-black text-[2.2rem] leading-[2.8rem] font-satoshi  smd:text-left">
           {title}
         </span>
         <motion.div
