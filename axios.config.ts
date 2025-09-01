51536c45fa5a4a3de8d32b7c5f71e99920d2ea50
ejim11 <favourejim56@gmail.com>
2025-09-01 12:59:26 +0100

    🔧 added timeouts for the axios base links

--- axios.config.ts
+++ axios.config.ts
@@ -7,69 +7,70 @@ const base =
 
 export const user = axios.create({
   baseURL: `${base}/users`,
-   timeout: 60000,
+  timeout: 60000,
 });
 
 export const auth = axios.create({
   baseURL: `${base}/auth`,
-   timeout: 60000,
+  timeout: 60000,
 });
 
 export const subscriber = axios.create({
   baseURL: `${base}/subscribers`,
-   timeout: 60000,
+  timeout: 60000,
 });
 
 export const paystack = axios.create({
   baseURL: `${base}/paystack`,
-   timeout: 60000,
+  timeout: 60000,
 });
 
 export const products = axios.create({
   baseURL: `${base}/products`,
-   timeout: 60000,
+  timeout: 60000,
 });
 
 export const subscribers = axios.create({
   baseURL: `${base}/subscribers`,
-   timeout: 60000,
+  timeout: 60000,
 });
 
 export const contactUs = axios.create({
   baseURL: `${base}/contact-us`,
-   timeout: 60000,
+  timeout: 60000,
 });
 
 export const discounts = axios.create({
   baseURL: `${base}/discounts`,
-   timeout: 60000,
+  timeout: 60000,
 });
 
 export const deliveryStates = axios.create({
   baseURL: `${base}/delivery-states`,
-   timeout: 60000,
+  timeout: 60000,
 });
 
 export const orders = axios.create({
   baseURL: `${base}/orders`,
-   timeout: 60000,
+  timeout: 60000,
 });
 
 export const productVariants = axios.create({
   baseURL: `${base}/product-variants`,
-   timeout: 60000,
+  timeout: 60000,
 });
 
 export const bankTransfer = axios.create({
   baseURL: `${base}/bank-transfers`,
-   timeout: 60000,
+  timeout: 60000,
 });
 
 export const reviews = axios.create({
   baseURL: `${base}/reviews`,
-   timeout: 60000,
+  timeout: 60000,
 });
 
 export const aggregate = axios.create({
   baseURL: `${base}/aggregate`,
+  timeout: 60000,
 });
