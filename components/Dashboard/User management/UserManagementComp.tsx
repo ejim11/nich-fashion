892e2c817aad348e2b6694e08c33332a604a1471
ejim11 <favourejim56@gmail.com>
2025-09-08 15:01:00 +0100

    ✅ added the user management comp

--- components/Dashboard/User management/UserManagementComp.tsx
+++ components/Dashboard/User management/UserManagementComp.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import HeaderText from "../HeaderText";
+import UsersTable from "./UsersTable";
+import ModalComp from "../ModalComp";
+import { AnimatePresence } from "framer-motion";
+import AddUserFormModal from "./AddUserFormModal";
 
 const UserManagementComp = () => {
-  return <div>UserManagementComp</div>;
+  const [modalIsVisible, setModalIsVisible] = useState<boolean>(false);
+
+  const openAddNewUserModalHandler = () => {
+    setModalIsVisible(true);
+  };
+
+  return (
+    <>
+      <div className="flex w-full items-center justify-between">
+        <HeaderText text="User Management" />
+        <button
+          type="button"
+          className="bg-black text-white rounded-[0.8rem] py-[1.6rem] px-[5.4rem]"
+          onClick={openAddNewUserModalHandler}
+        >
+          Add New User
+        </button>
+      </div>
+      <div className="mt-[3.6rem] w-full h-auto border border-[rgba(175,175,175,1)] flex-1 bg-[rgba(253,253,253,1)] rounded-[0.4rem] px-[4rem] py-[2.1rem] flex">
+        <UsersTable />
+      </div>
+      <AnimatePresence>
+        {modalIsVisible && (
+          <ModalComp setVisible={setModalIsVisible}>
+            <AddUserFormModal setModalIsVisible={setModalIsVisible} />
+          </ModalComp>
+        )}
+      </AnimatePresence>
+    </>
+  );
 };
 
 export default UserManagementComp;
